3e098db8b3df433327be1efb10c4268802bf24d3
saprovec <shkarobeynikov@yandex.ru>
2022-05-04 22:34:49 +0700

    animation

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,35 +1,72 @@
-import React from 'react';
+import React, {forwardRef} from 'react';
+import {motion} from "framer-motion";
 import classes from "./Features.module.scss";
 import {data} from "./data"
+
+const featuresAnimation = {
+    hidden : {
+        y: 100,
+        opacity: 0
+    },
+    visible: custom => ({
+        y: 0,
+        opacity: 1,
+        transition: { delay: custom * 0.2}
+    })
+}
 
 const Features = () => {
 
     return (
         <div className={classes.feature_content}>
             <div className={classes.features}>
-                <div className={classes.features__info}>
-                    <div className={classes.features__title}>{data.title}</div>
-                    <div className={classes.features__subtitle}>{data.subtitle}</div>
-                </div>
+                <motion.div
+                    className={classes.features__info}
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{amount: 0.4, once: true}}
+                >
+                    <motion.h2
+                        custom={1}
+                        variants={featuresAnimation}
+                        className={classes.features__title}
+                    >
+                        {data.title}
+                    </motion.h2>
+                    <motion.p
+                        custom={2}
+                        variants={featuresAnimation}
+                        className={classes.features__subtitle}
+                    >
+                        {data.subtitle}
+                    </motion.p>
+                </motion.div>
             </div>
-            <div className={classes.items}>
-                {data.features.map((feature) => <FeatureItem key={feature.id} {...feature}/>)}
-            </div>
+            <motion.div
+                className={classes.items}
+                initial="hidden"
+                whileInView="visible"
+                viewport={{amount: 0.1, once: true}}
+            >
+                {data.features.map((feature, index) =>
+                    <FeatureItem
+                        custom={index}
+                        variants={featuresAnimation}
+                        key={feature.id}
+                        {...feature}/>)}
+            </motion.div>
         </div>
     );
 }
 
 export {Features}
 
-const FeatureItem = ({id, title, text, img}) => {
-
-    return (
-        <div className={classes.item} style={{marginTop: id*45}}>
+const FeatureItem = motion(forwardRef(({id, title, text, img}, ref) => (
+        <div ref={ref} className={classes.item} style={{marginTop: id*45}}>
             <div className={classes.item__img}>
                 <img alt={title} title={title} src={img}/>
             </div>
-            <div className={classes.item__title}>{title}</div>
-            <div className={classes.item__text}>{text}</div>
+            <h3 className={classes.item__title}>{title}</h3>
+            <p className={classes.item__text}>{text}</p>
         </div>
-    );
-}
+)));
--- src/components/more/More.jsx
+++ src/components/more/More.jsx
@@ -1,16 +1,62 @@
 import React from 'react';
 import classes from "./More.module.scss";
 import {data} from "./data";
+import {motion} from "framer-motion";
+
+const moreAnimation = {
+    hidden : {
+        x: -100,
+        opacity: 0
+    },
+    visible: custom => ({
+        x: 0,
+        opacity: 1,
+        transition: { delay: custom * 0.2}
+    })
+};
+
+const scaleAnimation = {
+    hidden: {
+        scale: 0
+    },
+    visible: {
+        scale: 1,
+        transition: { delay: 0.2 },
+    }
+};
 
 const More = () => {
 
     return (
-        <div className={classes.more}>
-            <div className={classes.more__title}>{data.title}</div>
-            <div className={classes.more__text}>{data.text}</div>
-            <div className={classes.more__button}><a href="#" title="Get Started">Get Started</a></div>
-            <div className={classes.more__img}><img src="./img/illustration-working.svg"/></div>
-        </div>
+        <motion.div
+            className={classes.more}
+            initial="hidden"
+            whileInView="visible"
+            viewport={{once: true}}
+        >
+            <motion.h1
+                className={classes.more__title}
+                custom={1}
+                variants={moreAnimation}
+            >
+                {data.title}
+            </motion.h1>
+            <motion.div
+                className={classes.more__text}
+                custom={2}
+                variants={moreAnimation}
+            >
+                {data.text}
+            </motion.div>
+            <motion.div
+                className={classes.more__button}
+                custom={3}
+                variants={moreAnimation}
+            >
+                <a href="#" title="Get Started">Get Started</a>
+            </motion.div>
+            <motion.div variants={scaleAnimation} className={classes.more__img}><img src="./img/illustration-working.svg"/></motion.div>
+        </motion.div>
     );
 }
 
